4251afa3af6d006c4a93a22973a211a90510f7a5
weamosama <weam.hassanein@gmail.com>
2023-10-16 07:34:12 -0700

    test1

---
+++ main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_string(va_list argsList);
+int print_char(va_list argsList);
+
+int absalute(int num);
+int printdigit(int num);
+int printinteger(int num);
+int print_int(va_list
+	var);
+int handle_format_specifier(char specifier, va_list argsList);
+int print_binary(va_list argsList);
+#endif
