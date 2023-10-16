a621bd4d4871604f59ca88efd9e986619f36c6c6
jeonsho <jeonsho1999@gmail.com>
2023-10-16 08:10:04 -0700

    t

---
+++ print_int.c
@@ -0,0 +1,84 @@
+#include "main.h"
+
+/**
+ *absalute - calculates the absolute value.
+ *@num: input .
+ *Return: value.
+ */
+int absalute(int num)
+{
+	if (num < 0)
+		return (-1 * num);
+	else
+		return (num);
+}
+
+/**
+ *printdigit - counts the digits.
+ *@num: input integer
+ *Return: digit count
+ */
+int printdigit(int num)
+{
+	int count = 0;
+	int num2 = num;
+
+	if (num <= 0)
+		count += 1;
+
+	while (absalute(num2) != 0)
+	{
+		num2 = num2 / 10;
+		count++;
+	}
+
+	return (count);
+}
+
+/**
+ *printinteger - Prints the digits of an integer and returns the count.
+ *
+ *This function takes an integer as input and prints its individual digits
+ *The count of digits is then returned.
+ *
+ *@num: The integer to be printed.
+ *
+ *Return: The count of digits in the integer.
+ */
+int printinteger(int num)
+{
+	unsigned int byte;
+	int count;
+
+	count = printdigit(num);
+	if (num < 0)
+	{
+		_putchar('-');
+		byte = -num;
+	}
+	else
+		byte = num;
+
+	if (byte >= 10)
+		printinteger(byte / 10);
+	_putchar(byte % 10 + '0');
+
+	return (count);
+}
+
+/**
+ *print_int - Prints a number based on the specified format.
+ *@var: A `va_list` containing the number to be printed.
+ *
+ *Return: The number of characters printed .
+ */
+int print_int(va_list var)
+{
+	int number = va_arg(var, int);
+	int dig;
+
+	dig = printinteger(number);
+
+	return (dig);
+}
+
